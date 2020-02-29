a4d20694d096a333924c5bb67fe1c7668876581b
Yuki Yoshida <yuyoshd@amazon.com>
2020-02-29 21:24:40 +0900

    lambdaを更新

--- lambda/app/app.py
+++ lambda/app/app.py
@@ -6,5 +6,5 @@ def lambda_handler(event, context):
             "x-custom-header" : "my custom header value",
             "Access-Control-Allow-Origin": "my-origin.com"
         },
-        'body': '{"message": "Hello from AWS Lambda4"}'
+        'body': '{"message": "Hello from AWS Lambda"}'
     }
